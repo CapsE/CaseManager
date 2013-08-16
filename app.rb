d7d1adb8cdf4984f7dc7924e91d038c952003b84
CapsE <larskrafft@yahoo.de>
2013-08-16 13:14:09 +0200

    Style Update
    Lord Frogginton hinzugefügt,
    Kommentare geschrieben

--- app.rb
+++ app.rb
@@ -1,26 +1,24 @@
-# app.rb
+begin # Library imports
 
-require 'sinatra'
-require 'sinatra/activerecord'
-require './environments'
-require 'sinatra/flash'
-require 'sinatra/redirect_with_flash'
-require './runner'
-require './basic'
+	require 'sinatra'
+	require 'sinatra/activerecord'
+	require './environments'
+	require 'sinatra/flash'
+	require 'sinatra/redirect_with_flash'
+	require './runner'
+	require './basic'
 
-enable :sessions
-
-
-class Group < ActiveRecord::Base
+	enable :sessions
 end
 
-class Case < ActiveRecord::Base
-end
+begin # Klassen definitionen der Datenbank elemente
+	class Group < ActiveRecord::Base
+		validates :elements, presence: true, length: { minimum: 2 }
+	end
 
-class Gc_relation < ActiveRecord::Base
-end
-
-class Gg_relation < ActiveRecord::Base
+	class Case < ActiveRecord::Base
+		validates :code, presence: true
+	end
 end
 
 get "/" do
@@ -30,94 +28,8 @@ get "/" do
   erb :"posts/index"
 end
 
-get "/caseses/create" do
-  @title = "Create Case"
-  @case = Case.new
-  erb :"caseses/create"
-end
-
-get "/caseses/edit/:id" do
-	@title = "Edit Case"
-	@case = Case.find(params[:id])
-	erb:"caseses/edit"
-end
-
-get "/groups/create" do
-  @groups = Group.order("created_at DESC")
-  @caseses = Case.order("created_at DESC")
-  @title = "Create Group"
-  @group = Group.new
-  erb :"groups/create"
-end
-
-get "/groups/edit/:id" do
-	@title = "Edit Groups"
-	@groups = Group.order("created_at DESC")
-	@caseses = Case.order("created_at DESC")
-	@group = Group.find(params[:id])
-	erb:"groups/edit"
-end
-
-post "/groups/edit" do
-	@group = Group.update(params[:post][:id], params[:post])
-	 if @group.save
-		redirect "/"
-	  else
-		redirect "/groups/edit/#{params[:id]}", :error => 'Something went wrong. Try again.'
-	  end
-end
-
-get "/groups/delete/:id" do
-	Group.delete(params[:id])
-	redirect "/"
-end
-
-get "/caseses/delete/:id" do
-	Case.delete(params[:id])
-	redirect "/"
-end
-
-post "/caseses" do
-  @case = Case.new(params[:post])
-  if @case.save
-    redirect "/"
-  else
-    redirect "caseses/create", :error => 'Something went wrong. Try again.'
-  end
-end
-
-post "/caseses/edit" do
-	@case = Case.update(params[:post][:id], params[:post])
-	 if @case.save
-		redirect "/"
-	  else
-		redirect "/caseses/edit/#{params[:id]}", :error => 'Something went wrong. Try again.'
-	  end
-end
-
-post "/groups" do
-	@group = Group.new(params[:post])
-	
-	if @group.save
-		redirect "/"
-	else
-		redirect "groups/create", :error => 'Something went wrong. Try again.'
-	end
-end
-
 get "/result" do
 	erb :result
-end
-
-get "/posts/:id" do
-  @post = Post.find(params[:id])
-  @title = @post.title
-  erb :"posts/view"
-end
-
-class Post < ActiveRecord::Base
-  validates :title, presence: true, length: { minimum: 5 }
-  validates :body, presence: true
 end
 
 helpers do
@@ -129,3 +41,101 @@ helpers do
     end
   end
 end
+
+begin # Erstellen von Caseses und Groups
+	# Interface Caseses
+	get "/caseses/create" do
+		@title = "Create Case"
+		@case = Case.new
+		erb :"caseses/create"
+	end
+	
+	# In die Datenbank ablegen
+	post "/caseses" do
+		@case = Case.new(params[:post])
+		if @case.save
+			redirect "/"
+		else
+			redirect "caseses/create", :error => 'Something went wrong. Try again.'
+		end
+	end
+	
+	# Interface Groups
+	get "/groups/create" do
+		@groups = Group.order("created_at DESC")
+		@caseses = Case.order("created_at DESC")
+		@title = "Create Group"
+		@group = Group.new
+		erb :"groups/create"
+	end
+	
+	# In die Datenbank ablegen
+	post "/groups" do
+		@group = Group.new(params[:post])
+		
+		if @group.save
+			redirect "/"
+		else
+			redirect "groups/create", :error => 'Something went wrong. Try again.'
+		end
+	end
+
+end
+
+begin # Editieren von Caseses und Groups
+	
+	# Interface Case editieren
+	get "/caseses/edit/:id" do
+		@title = "Edit Case"
+		@case = Case.find(params[:id])
+		erb:"caseses/edit"
+	end
+	
+	# In die Datenbak eintragen
+	post "/caseses/edit" do
+		@case = Case.update(params[:post][:id], params[:post])
+		 if @case.save
+			redirect "/"
+		  else
+			redirect "/caseses/edit/#{params[:id]}", :error => 'Something went wrong. Try again.'
+		  end
+	end
+	
+	# Interface Group editieren
+	get "/groups/edit/:id" do
+		@title = "Edit Groups"
+		@groups = Group.order("created_at DESC")
+		@caseses = Case.order("created_at DESC")
+		@group = Group.find(params[:id])
+		erb:"groups/edit"
+	end
+	
+	# In die Datenbak eintragen
+	post "/groups/edit" do
+		@group = Group.update(params[:post][:id], params[:post])
+		 if @group.save
+			redirect "/"
+		  else
+			redirect "/groups/edit/#{params[:id]}", :error => 'Something went wrong. Try again.'
+		  end
+	end
+end
+
+begin # Löschen von Cases und Groups
+
+	# Delete Case
+	get "/caseses/delete/:id" do
+		Case.delete(params[:id])
+		redirect "/"
+	end
+	
+	# Delete Group
+	get "/groups/delete/:id" do
+		Group.delete(params[:id])
+		redirect "/"
+	end
+
+end
+
+
+
